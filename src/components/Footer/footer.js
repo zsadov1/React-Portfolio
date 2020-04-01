1394441a729c6cfe22953b512720e97960e6af52
zsadov1 <zsadov1@gmail.com>
2020-04-01 12:40:47 -0400

    Footer Component update

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+	return (
+		<footer id='lab_social_icon_footer'>
+			<div class='text-center center-block'>
+				<a href='https://www.linkedin.com/in/zachary-sadovszky-b10a60196/'>
+					<i class='fab fa-linkedin fa-3x'></i>
+				</a>
+				<a href='https://github.com/zsadov1'>
+					<i class='fab fa-github fa-3x'></i>
+				</a>
+				<a href='mailto:zsadov1@gmail.com'>
+					<i class='far fa-envelope fa-3x'></i>
+				</a>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
