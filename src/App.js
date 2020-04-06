31b30a8d7bc4a9c352281f99d59419cdd75c09c3
zsadov1 <zsadov1@gmail.com>
2020-04-06 18:25:33 -0400

    About me Update.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Navbar from "./components/Navbar/navbar";
 import Header from "./components/Header/header";
 import Footer from "./components/Footer/footer";
 
-import About from "./pages/AboutMe/aboutme";
 import Projects from "./pages/Projects/projects";
 import Resume from "./pages/Resume/resume";
+import AboutMe from "./pages/AboutMe/aboutme.js";
 
 import "./app.css";
 
@@ -18,7 +18,7 @@ function App() {
 					<div className='ContentDiv'>
 						<Navbar />
 						<Header />
-						<Route exact path='/' component={About} />
+						<Route exact path='/' component={AboutMe} />
 						<Route exact path='/projects' component={Projects} />
 						<Route exact path='/resume' component={Resume} />
 					</div>
