e922ad57c074f0214469fde069e5d87edd1f0132
zsadov1 <zsadov1@gmail.com>
2020-04-07 16:12:30 -0400

    Deployment issues

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,9 +1,5 @@
 import React from "react";
-// import Image from "react-image-resizer";
 import Serious from "../Header/serious.png";
-import Question from "../Header/question.png";
-import Silly from "../Header/silly.png";
-import Excited from "../Header/excited.png";
 
 import "./header.css";
 
