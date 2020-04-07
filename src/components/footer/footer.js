b50836de3a1f51f51198362efb32f0ccdc20cc34
zsadov1 <zsadov1@gmail.com>
2020-04-07 18:22:30 -0400

    Deploy

---
+++ src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import "./footer.css";
+
+function Footer() {
+	return (
+		<footer>
+			<p className='text-center'>&copy; 2020 Zach Sadovszky • (804) 363-6102</p>
+		</footer>
+	);
+}
+
+export default Footer;
---
+++ src/components/header/header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Serious from "../header/serious.png";
+
+import "./header.css";
+
+function Header() {
+	return (
+		<>
+			<div className='flex-container'>
+				<img
+					src={Serious}
+					className='img-fluid headshot'
+					alt='Serious Headshot'
+				/>
+			</div>
+		</>
+	);
+}
+
+export default Header;
